2439699c3fee1d64a35d0cbcf3ad7bef015267a1
Dmitriy Iskra <dmitriyiskra@mail.ru>
2025-03-27 15:52:45 +0300

    added file deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e # если один из запущенных процессов закончится ошибкой скрипт прекратит свою работу
+
+
+git pull origin main
+
+/opt/php82/bin/php artisan down
+
+/opt/php82/bin/php composer.phar install --no-dev --optimize-autoloader
+
+/opt/php82/bin/php artisan migrate # возможно понадобится --force потому что проект в продакшн
+
+/opt/php82/bin/php artisan config:cache
+
+/opt/php82/bin/php artisan view:cache
+
+/opt/php82/bin/php artisan event:cache
+
+/opt/php82/bin/php artisan route:cache
+
+/opt/php82/bin/php artisan up
